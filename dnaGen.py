0c231bb8b47628b10e62412fe5c5113ba845a86b
lucaskf1996 <lucaskf1996@gmail.com>
2022-06-03 11:23:10 +0000

    paralelo

--- dnaGen.py
+++ dnaGen.py
@@ -1,21 +1,21 @@
 import random
-n=1000
-m=1000
-v=1
-while(n<=10000):
-     while(m<=10000):
-          while(v<3):
-               file = f"{n:05d}n{m:05d}m{v}.seq"
-               f = open(file, 'w')
-               seq=[str(n)+'\n',
-                    str(m)+'\n',
-                    ''.join(random.choices(['A','T','C','G','-'],k=n))+'\n',
-                    ''.join(random.choices(['A','T','C','G','-'],k=m))]
-               f.writelines(seq)
-               f.close()
-               v+=1
-          v=1
-          m+=2000
-     m=1000
-     n+=1000
+n=10
+m=10
+v=2
+while(n<=100):
+     while(m<=100):
+          # while(v<3):
+          file = f"{n:03d}n{m:03d}m{v}.seq"
+          f = open(file, 'w')
+          seq=[str(n)+'\n',
+               str(m)+'\n',
+               ''.join(random.choices(['A','T','C','G','-'],k=n))+'\n',
+               ''.join(random.choices(['A','T','C','G','-'],k=m))]
+          f.writelines(seq)
+          f.close()
+          #      v+=1
+          # v=1
+          m+=10
+     m=10
+     n+=10
 
--- testes.py
+++ testes.py
@@ -2,15 +2,24 @@ import subprocess
 import time
 import os
 
-files = os.listdir("./tests")
+files = os.listdir("./smaller_tests")
 files.sort()
-# print(files)
-resultados = open("./resultados.txt", "a")
+resultados = open("./resultados/exhaustiveParallel.txt", "a")
 for file in files:
-    # print(file)
-    with open("./tests/"+file) as f:
+    with open("./smaller_tests/"+file) as f:
+        print(file)
         start = time.perf_counter()
-        proc = subprocess.run(['./parallelLocal'], input=f.read(), text=True, capture_output=True)
+        proc = subprocess.run(['./exhaustiveParallel'], input=f.read(), text=True, capture_output=True)
         end = time.perf_counter()
         print(end - start)
         resultados.write(f"{end - start}\n")
+resultados.close()
+
+# resultados = open("./resultados/exhaustiveParallel.txt", "a")
+# with open("./smaller_tests/010n010m2.seq") as f:
+#     start = time.perf_counter()
+#     proc = subprocess.run(['./exhaustiveParallel'], input=f.read(), text=True, capture_output=True)
+#     end = time.perf_counter()
+#     print(end - start)
+#     resultados.write(f"{end - start}\n")
+# resultados.close()
